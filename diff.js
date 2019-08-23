51a58f70475592a2697964429ac6b108e0004baf
anhnt <anhntbk08@gmail.com>
2019-08-23 10:16:50 +0700

    [Fix] refactor code and some adjustment on command

--- diff.js
+++ diff.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose'),
     ENV = require("./env.json"),
     Excel = require('exceljs');
 
+mongoose.pluralize(null);
 mongoose.connect(ENV.MONGODB_URI, { useNewUrlParser: true });
 
 var accountCanonical = new Schema({
@@ -18,7 +19,7 @@ var accountCanonical = new Schema({
     storageRoot: String,
     codeHash: String
 }, {
-        timestamps: false
+        timestamps: false,
     });
 
 var accountForkPath = new Schema({
@@ -28,14 +29,13 @@ var accountForkPath = new Schema({
     storageRoot: String,
     codeHash: String
 }, {
-        timestamps: false
+        timestamps: false,
     });
 
 
 var canonical = mongoose.model('canonical-accounts', accountCanonical);
 var forkpath = mongoose.model('fork-accounts', accountForkPath);
 
-var injectedAccounts = [];
 var ether = 1000000000000000000;
 
 async function findAccountsThatHasBalanceMoreThan1000(collection) {
@@ -61,7 +61,7 @@ async function findAccountsThatHasBalanceMoreThan1000(collection) {
 async function findDiff() {
     let wrongAccounts = await findAccountsThatHasBalanceMoreThan1000(canonical);
     let realAccounts = await findAccountsThatHasBalanceMoreThan1000(forkpath);
-
+    let injectedAccounts = [];
     let listDiff = _.differenceWith(wrongAccounts, realAccounts, _.isEqual);
 
     for (let index = 0; index < listDiff.length; index++) {
@@ -87,33 +87,36 @@ async function writeToExcel(accounts) {
     const options = {
         filename: ENV.REPORT_OUTPUT_NAME,
         useStyles: true,
-        useSharedStrings: trueaccounts
+        useSharedStrings: true
     };
-
     const workbook = new Excel.stream.xlsx.WorkbookWriter(options);
-
     const worksheet = workbook.addWorksheet('Report');
 
     worksheet.columns = [
         { header: '#', key: 'index' },
-        { header: 'SHA3(ACCOUNT)', key: 'keyHash' },
+        { header: 'SHA3(ACCOUNT)', key: '_id' },
         { header: 'Hacked Balance', key: 'tomo' },
         { header: 'Origin Balance', key: 'realBalance' }
     ]
 
     for (let index = 1; index <= accounts.length; index++) {
         let data = {
-            id: index,
+            index: index,
             ...accounts[index]
-        };accounts
+        };
 
         worksheet.addRow(data).commit();
     }
 
     workbook.commit().then(function () {
         console.log('excel file created');
+        process.exit(0);
     });
 }
 
-const injectedAccounts = await findDiff();
-writeToExcel(injectedAccounts);
+async function main() {
+    const injectedAccounts = await findDiff();
+    writeToExcel(injectedAccounts);
+}
+
+main()
